5cf75f533df611ae42368a9a89a590afb9029e22
IrynaChelombitko <ang.feofan@gmail.com>
2022-12-25 04:00:11 +0200

    add

--- src/script.js
+++ src/script.js
@@ -75,7 +75,7 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-
+  let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
@@ -85,7 +85,7 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -114,5 +114,3 @@ currentLocationButton.addEventListener("click", getCurrentPosition);
 
 let searchForm = document.querySelector("#city-input");
 searchForm.addEventListener("submit", searchCity);
-
-search("New York");
